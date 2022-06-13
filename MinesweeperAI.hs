7435f05fb796ae9fdc52f3d985be01d313205d59
Omar-Nour <omarnour21on@gmail.com>
2022-06-13 23:12:29 +0200

    Style Adjust

--- MinesweeperAI.hs
+++ MinesweeperAI.hs
@@ -6,19 +6,21 @@ data MyState = Null | S Cell [Cell] String MyState deriving (Show)
 up:: MyState -> MyState
 up Null = Null
 up (S (y,x) mines prevAction state) | y <= 0 = Null
-                                      | otherwise = (S (y-1,x) mines "up" ((S (y,x) mines prevAction state)))
+                                    | otherwise = (S (y-1,x) mines "up" ((S (y,x) mines prevAction state)))
 
 down:: MyState -> MyState
 down Null = Null
-down (S (y,x) mines prevAction state) | y >= 3 = Null
-                                        | otherwise = (S (y+1,x) mines "down" (S (y,x) mines prevAction state))
+down (S (y,x) mines prevAction state) | y >= 3 = Nulls
+                                      | otherwise = (S (y+1,x) mines "down" (S (y,x) mines prevAction state))
 
 left:: MyState -> MyState
 left Null = Null
 left (S (y,x) mines prevAction state) | x <= 0 = Null
-                                        | otherwise = (S (y,x-1) mines "left" ((S (y,x) mines prevAction state)))
+                                      | otherwise = (S (y,x-1) mines "left" ((S (y,x) mines prevAction state)))
 
 right:: MyState -> MyState
 right Null = Null
 right (S (y,x) mines prevAction state) | x >= 3 = Null
-                                         | otherwise = (S (y,x+1) mines "right" ((S (y,x) mines prevAction state)))
+                                       | otherwise = (S (y,x+1) mines "right" ((S (y,x) mines prevAction state)))
+
+                                      
