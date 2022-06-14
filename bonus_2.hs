5af7058d2eb8a6beb9937fca7df4cd2c25a0f125
Mohamed Shamekh <shamekhjr@gmail.com>
2022-06-14 13:21:05 +0200

    implemented some unused methods

--- bonus_2.hs
+++ bonus_2.hs
@@ -36,6 +36,18 @@ collect (S curr mines prevAction state n m) | elem curr mines = (S curr (delete 
 nextMyStates::MyState->[MyState]
 nextMyStates (S curr mines prevAction state n m) =   filter (/=Null) [(up (S curr mines prevAction state n m)), (left (S curr mines prevAction state n m)), (right (S curr mines prevAction state n m)), (down (S curr mines prevAction state n m)),(collect (S curr mines prevAction state n m))]
 
+
+manhattan (y,x) (y2,x2) = abs(y-y2)+abs(x-x2)
+
+-- they work
+closest (S (y,x) ((yMine,xMine):t) prevAction state n m) = closestMine (S (y,x) ((yMine,xMine):t) prevAction state n m) (n*m) (n,m)
+closestMine (S (y,x) [] prevAction state n m) minDist minMine = minMine
+closestMine (S (y,x) ((yMine,xMine):t) prevAction state n m) minDist minMine
+  | manhattan (y,x) (yMine,xMine) < minDist = closestMine (S (y,x) t prevAction state n m) (manhattan (y,x) (yMine,xMine)) (yMine,xMine)
+  | otherwise = closestMine (S (y,x) t prevAction state n m) minDist minMine
+
+--closestState closest_mine ((S curr mines prevAction state n m):t) = closest
+
 isGoal::MyState->Bool
 isGoal Null = False
 isGoal (S curr mines prevAction state n m) =  mines == []
